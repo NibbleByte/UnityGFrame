b84c214ff09b2185aa41640ea4cd12b9bd9e67b3
Filip Slavov <NibbleByte3@gmail.com>
2023-04-02 00:07:14 +0300

    * GFrame: Adding hotkey component to focus UIScope. Applied it to the test scene - options example.

---
+++ Packages/DevLocker.GFrame/Input/UIScopes/HotkeyFocusScopeElement.cs
@@ -0,0 +1,32 @@
+#if USE_INPUT_SYSTEM
+
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DevLocker.GFrame.Input.UIScope
+{
+	/// <summary>
+	/// Calls UnityEvent on specified InputAction.
+	/// Note that this action has to be enabled in order to be invoked.
+	/// </summary>
+	public class HotkeyFocusScopeElement : HotkeyBaseScopeElement
+	{
+		public UIScope FocusScope;
+
+		protected override void OnInvoke()
+		{
+			if (FocusScope == null) {
+				FocusScope = GetComponentInParent<UIScope>();
+			}
+
+			if (FocusScope == null) {
+				Debug.LogWarning($"No scope to focus for {name}", this);
+				return;
+			}
+
+			FocusScope.ForceRefocusScope();
+		}
+	}
+}
+
+#endif
