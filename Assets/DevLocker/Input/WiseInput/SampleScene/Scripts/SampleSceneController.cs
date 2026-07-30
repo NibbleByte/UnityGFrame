3789abe15b3652f7ae83317d842d77b7f699238c
NibbleByte <NibbleByte3@gmail.com>
2026-07-30 22:20:30 +0300

    Renamed WiseInput assembly. SampleSceneController now uses InputCollectionContext by default.

--- Assets/DevLocker/Input/WiseInput/SampleScene/Scripts/SampleSceneController.cs
+++ Assets/DevLocker/Input/WiseInput/SampleScene/Scripts/SampleSceneController.cs
@@ -14,6 +14,8 @@ namespace DevLocker.WiseInput.Sample
 	{
 		public static SampleSceneController Instance { get; private set; }
 
+		[Tooltip("If true, will use the PlayerInput component to manage the input context.\nIf false, will use better custom implementation.\n\nRecommended: avoid using the PlayerInput component.")]
+		public bool UsePlayerInputComponent = false;
 		public GameObject GameInputPrefab;
 		public InputBindingDisplayAsset[] BindingDisplayAssets;
 
@@ -37,23 +39,37 @@ namespace DevLocker.WiseInput.Sample
 			var gameInputObject = Instantiate(GameInputPrefab, transform);
 
 			gameInputObject.name = gameInputObject.name.Replace("(Clone)", "-Global");
-			var playerInput = gameInputObject.GetComponentInChildren<PlayerInput>();
 
-			// HACK: trick the PlayerInput to use the reference to our asset instead of copying the actions. Check the InputComponentContext() constructor for more info.
-			// NOTE: the PlayerInput must initially have empty reference set for InputActionAsset in the prefab.
-			playerInput.enabled = false;
-			playerInput.actions = PlayerControls.asset;
-			playerInput.enabled = true;
+			// NOTE: There are two IInputContext implementations. One uses PlayerInput which is highly discouraged as it has issues and you should be using this IInputContext instead for everything.
+			if (UsePlayerInputComponent) {
+				var playerInput = gameInputObject.GetComponentInChildren<PlayerInput>();
 
-			var uiInputModule = gameInputObject.GetComponentInChildren<InputSystemUIInputModule>();
-			uiInputModule.actionsAsset = PlayerControls.asset;  // This will refresh the UI Input action references to the new asset.
+				// HACK: trick the PlayerInput to use the reference to our asset instead of copying the actions. Check the InputComponentContext() constructor for more info.
+				// NOTE: the PlayerInput must initially have empty reference set for InputActionAsset in the prefab.
+				playerInput.enabled = false;
+				playerInput.actions = PlayerControls.asset;
+				playerInput.enabled = true;
 
-			playerInput.uiInputModule = uiInputModule;
+				var uiInputModule = gameInputObject.GetComponentInChildren<InputSystemUIInputModule>();
+				uiInputModule.actionsAsset = PlayerControls.asset;  // This will refresh the UI Input action references to the new asset.
 
-			var inputContext = new InputComponentContext(playerInput, new InputActionsMaskedStack(PlayerControls), IInputContext.InputBehaviours.Default, BindingDisplayAssets);
-			PlayerControls.SetInputContext(inputContext);
+				playerInput.uiInputModule = uiInputModule;
 
-			InputUIRootObject.GlobalUIRoot.SetupGlobal(uiInputModule.GetComponent<EventSystem>(), inputContext);
+				var inputContext = new InputComponentContext(playerInput, new InputActionsMaskedStack(PlayerControls), IInputContext.InputBehaviours.Default, BindingDisplayAssets);
+				PlayerControls.SetInputContext(inputContext);
+
+				InputUIRootObject.GlobalUIRoot.SetupGlobal(uiInputModule.GetComponent<EventSystem>(), inputContext);
+
+			} else {
+
+				var uiInputModule = gameInputObject.GetComponentInChildren<InputSystemUIInputModule>();
+				uiInputModule.actionsAsset = PlayerControls.asset;  // This will refresh the UI Input action references to the new asset.
+
+				var inputContext = new InputCollectionContext(PlayerControls, PlayerControls.Sample_UI.Get(), IInputContext.InputBehaviours.Default, BindingDisplayAssets);
+				PlayerControls.SetInputContext(inputContext);
+
+				InputUIRootObject.GlobalUIRoot.SetupGlobal(uiInputModule.GetComponent<EventSystem>(), inputContext);
+			}
 
 			// The whole level is UI, so enable it for the whole level.
 			PlayerControls.Enable(this, PlayerControls.Sample_UI);
